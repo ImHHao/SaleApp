06bb7a512eb1f85d4267f76015730277f3731352
ImHHao <2254052025hao@ou.edu.vn>
2024-10-26 18:29:32 +0700

    message

--- dao.py
+++ dao.py
@@ -3,3 +3,6 @@ import json
 def load_categories():
     with open('data/categories.json', encoding='utf-8') as f:
         return json.load(f)
+
+if __name__ == '__main__':
+    print('Hello World')
